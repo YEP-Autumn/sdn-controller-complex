60bfb6af1d942a1fac4596332ea066e029bb541b
YEP-Autumn <2017248646s@gmail.com>
2025-12-05 06:55:25 -0500

    update some file

--- lib/pcap_mk/pcap_mk.py
+++ lib/pcap_mk/pcap_mk.py
@@ -7,28 +7,28 @@ class PcapMk:
 
     pkt = None
 
-    def add_ethernet(self):
+    def add_ethernet(self, src, dst):
 
         if self.pkt:
-            self.pkt = self.pkt / Ether()
+            self.pkt = self.pkt / Ether(src=src, dst=dst)
         else:
-            self.pkt = Ether()
+            self.pkt = Ether(src=src, dst=dst)
 
         return self
 
-    def add_vlan(self):
+    def add_vlan(self, vlan):
         if self.pkt:
-            self.pkt = self.pkt / Dot1Q()
+            self.pkt = self.pkt / Dot1Q(vlan=vlan)
         else:
-            self.pkt = Dot1Q()
+            self.pkt = Dot1Q(vlan=vlan)
 
         return self
 
-    def add_ipv4(self):
+    def add_ipv4(self, src, dst):
         if self.pkt:
-            self.pkt = self.pkt / IP()
+            self.pkt = self.pkt / IP(src=src, dst=dst)
         else:
-            self.pkt = IP()
+            self.pkt = IP(src=src, dst=dst)
 
         return self
 
@@ -37,9 +37,11 @@ class PcapMk:
         self.pkt = None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pcap = PcapMk()
-    pcap.add_ethernet().add_vlan().add_ipv4().make('../../resource/auto_gen.pcap')
+    pcap.add_ethernet("00:11:22:33:44:05", "00:11:22:33:44:06").add_vlan(100).add_ipv4(
+        "1.2.3.4", "2.2.2.2"
+    ).make("../../resource/auto_gen.pcap")
 
     # a = rdpcap('../../resource/test.pcap')
     # print(f'${a[0]}')
